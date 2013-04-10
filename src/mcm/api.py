0010823fbc193514b51f301df51785970a18b10b
b3ni <brarcos@gmail.com>
2013-04-10 15:50:07 +0200

    row

--- src/mcm/api.py
+++ src/mcm/api.py
@@ -6,7 +6,8 @@ BASE = 'https://www.magiccardmarket.eu/'
 
 
 def search(query, lang='en'):
-    qs = {'mainPage': 'showSearchr', 'searchFor': query, 'resultsPage': 0}
+    # http://lxml.de/xpathxslt.html
+    qs = {'mainPage': 'showSearchResult', 'searchFor': query, 'resultsPage': 0}
 
     r = requests.get(BASE, params=qs)
     r.raise_for_status()
@@ -22,26 +23,48 @@ def search(query, lang='en'):
         m = re.search('resultsPage=(\d+)', href[0])
         npages = int(m.group(1)) + 1
 
+    # serach table
+    tree = tree.xpath("//table[contains(@class, 'SearchTable')]/tbody")
+    if len(tree) == 0:
+        return
+    tree = tree[0].getroottree()
+
+    # for e in tree.getroottree().iter():
+    #     print tree.getpath(e)
+
     pagenow = 1
     while pagenow <= npages:
         print "PAGE: {0}/{1}".format(pagenow, npages)
 
-        for row in tree.xpath("//*[@id=\"siteContents\"]/div/table/tbody/tr[contains(@class, 'row_')]"):
-            result = {'img': '', 'rarity': '', 'name': ''}
+        rows = tree.xpath("/tr[contains(@class, 'row_')]")
 
-            img = row.xpath("//td[1]//img/@onmouseover")
-            if img:
-                print img
-                m = re.search("'(.+?)'", img[0])
+        print rows
+        break
+        for row in rows:
+            result = {'img': '', 'expansion': '', 'rarity': '', 'name': '', 'id': ''}
+
+            data = row.xpath("/td[1]/img/@onmouseover")
+            if data:
+                m = re.search("'(.+?)'", data[0])
                 result['img'] = m.group(1)
 
-            # rarity = row.xpath("//td[2]//img/@onmouseover")
-            # if rarity:
-            #     pass
+            data = row.xpath("/td[2]/span/@onmouseover")
+            if data:
+                m = re.search("'(.+?)'", data[0])
+                result['expansion'] = m.group(1)
 
-            # name = row.xpath("//td[3]//img/@onmouseover")
-            # if name:
-            #     pass
+            data = row.xpath("/td[3]/img/@onmouseover")
+            if data:
+                m = re.search("'(.+?)'", data[0])
+                result['rarity'] = m.group(1)
+
+            data = row.xpath("/td[5]/a")
+            if data:
+                result['id'] = data[0].attrib['href']
+
+                # result['id'] = data.
+                # m = re.search("'(.+?)'", data[0])
+                # result['rarity'] = m.group(1)
 
             yield result
 
