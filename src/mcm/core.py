b337cb0f8a143122c67a01c9a537d49f553de3d8
unknown <brarcos@gmail.com>
2013-04-14 19:34:05 +0200

    modelos y otras cosas

--- src/mcm/core.py
+++ src/mcm/core.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 import mechanize
 import cookielib
+import error
+import models
 
 import codecs
 import datetime
 import logging
 import sys
+import re
 from lxml import etree
 
 logger = logging.getLogger("mechanize")
@@ -24,9 +27,21 @@ class MCMApi(object):
         self.cj = cookielib.LWPCookieJar()
 
         self.br.set_cookiejar(self.cj)
+
+        self.br.set_handle_equiv(True)
+        self.br.set_handle_gzip(True)
+        self.br.set_handle_redirect(True)
+        self.br.set_handle_referer(True)
         self.br.set_handle_robots(False)
+
+        # follows refresh 0 but not hangs on refresh > 0
         self.br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
         self.br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; es-VE; rv:1.9.0.1)Gecko/2008071615 Debian/6.0 Firefox/9')]
+
+        # debug
+        #self.br.set_debug_http(True)
+        #self.br.set_debug_redirects(True)
+        #self.br.set_debug_responses(True)
 
         self.br.open(self.base)
 
@@ -36,16 +51,44 @@ class MCMApi(object):
         self.br['userPassword'] = self.password
         self.br.submit()
 
-        print self.br.response().read()
-        self._page()
+        r = self.br.response().read()
+        m = re.search(r"The password you entered was not correct", r)
+        if m:
+            raise error.LoginError()
 
     def get_wants_list(self):
-        page = self._page({'mainPage': 'showWants'})
+        link = self.br.find_link(url="/?mainPage=showWants")
+        self.br.follow_link(link)
+
+        # read wants lists
+        wants_lists = []
+        utf8_parser = etree.HTMLParser(encoding='utf-8')
+        tree = etree.fromstring(self.br.response().read().decode('utf-8'), parser=utf8_parser)
+        for wlnode in tree.xpath('//select[@name="sw_WantsListID"]/option'):
+            name = re.search(r"([\w\s]+) ", wlnode.text)
+            wl = models.WantList(int(wlnode.attrib['value']), name.group(1))
+
+            # read wants card
+            self.br.open(self.base + wl.url())
+            tree = etree.fromstring(self.br.response().read().decode('utf-8'), parser=utf8_parser)
+            for wantnode in tree.xpath('//table[contains(@class, "wantsTable")]/tbody/tr'):
+                node = wantnode.xpath('td[3]/a')[0]
+                card = models.Card(node.attrib['href'], node.text)
+
+                node = wantnode.xpath('td[11]')[0]
+                want = models.Want(card, int(node.text))
+
+                wl.wants.append(want)
+
+            wants_lists.append(wl)
+
+        return wants_lists
+
+    def get_cart(self):
+        pass
 
     def _page(self):
         text = self.br.response().read().decode('utf-8')
-
-        print type(text)
         t = datetime.datetime.now()
         with codecs.open("last{0}{1}{2}_{3}{4}{5}.html".format(t.year, t.month, t.day, t.hour, t.minute, t.second), "w", "utf-8") as f:
             f.write(text)
@@ -54,4 +97,6 @@ if __name__ == '__main__':
     mcm = MCMApi(username='xxx', password='xxx')
 
     mcm.login()
-    #mcm.get_wants_list()
+
+    for wl in mcm.get_wants_list():
+        print wl
---
+++ src/mcm/models.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+
+
+class Card:
+    def __init__(self, id, name=""):
+        self.id = id
+        self.name = name
+
+        self.is_metacard = id.find('idMetacard') != -1
+
+    def url(self):
+        return "{0}".format(self.id)
+
+
+class Want:
+    def __init__(self, card, amount=1):
+        self.card = card
+        self.amount = amount
+
+
+class WantList:
+    def __init__(self, id, name):
+        self.id = id
+        self.name = name
+        self.wants = []
+
+    def url(self):
+        return "?mainPage=showWants&idWantsList={0}".format(self.id)
+
+    def __str__(self):
+        return "<WantList {0}> {1} ({2})".format(self.id, self.name, len(self.wants))
+
+
+class Seller:
+    def __init__(self, name):
+        self.name = name
+
+
+class CardArticle:
+    self __init__(self, card, price, language, expansion, condition, quantity=1):
+        self.card = card
+        self.price = price
+        self.language = language
+        self.expansion = expansion
+        self.condition = condition
+        self.quantity = quantity
+
+
+class Ship:
+    def __init__(self, seller):
+        self.seller = seller
+        self.shipping = 0
+        self.shipping_method = ''
+        self.articles = []
+
+    def total(self):
+        return 0
+
+
+class Cart:
+    def __init__(self):
+        self.ships = []
+
+    def total(self):
+        return 0
